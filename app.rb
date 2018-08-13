1859fca54d8d2bd1f054d563364af417eb9ccba1
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-13 16:41:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @text = params[:user_input]
+    @text = params[:user_phrase]
     @data = PigLatinizer.new
     erb :piglatinize
   end
