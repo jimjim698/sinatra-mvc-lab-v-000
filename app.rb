b92b885e47f62ba3aaf8129d221240a72d1ebd43
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-13 16:11:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/pl' do
+
+    @data = PigLatinizer.new(params[:user_input])
+    erb :pl
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class PigLatinizer
+
+  attr_reader :text
+
+  def initialize(text = nil)
+
+    @text = text
+
+  end
+
+def full_sentence
+  sent_array = @text.split(" ")
+  sent_array.collect do |word|
+    piglatinize(word)
+  end
+
+end
+
+  def vowel?(word)
+    word = word.split("")
+     word.detect do |let|
+      let.downcase == "a" || let.downcase == "e" || let.downcase == "i" || let.downcase == "o" || let.downcase == "u"
+    end
+  end
+
+
+
+def piglatinize(sent)
+  sent_array = sent.split(" ")
+  let = word.split("")
+  word.each do |let|
+  if vowel?(let[0]) && let[0] == "e" &&  let[2] == "n"
+    w2 = let.join("")
+    w2 + "way"
+  elsif
+    vowel?(let[0])
+    w2 = let.join("")
+    w2 + "way"
+  elsif
+    let.size > 1
+  while !vowel?(let[0])
+    let << let.shift
+  end
+    w2 = let.join("")
+    w2 + "ay"
+  else
+    w2 = let.join("")
+    w2 + "way"
+end 
+end
+end
+
+
+
+
+end
